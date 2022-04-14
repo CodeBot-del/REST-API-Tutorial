ce94a723bb37a3c5b6f8863b1c7189ba174055c5
gracie2 <stevenmanangu360@gmail.com>
2022-04-14 11:06:11 +0300

    implemented list api view

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,15 +3,15 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-# product create view 
-class ProductCreateAPIView(generics.CreateAPIView):
+# product create API view 
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
     # in case you wanna assign something when the product is created
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
-        print(serializer.validated_data)  #serializer.validated_data returns the data thats coming from the user request
+        # print(serializer.validated_data)  #serializer.validated_data returns the data thats coming from the user request
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
@@ -19,13 +19,25 @@ class ProductCreateAPIView(generics.CreateAPIView):
         
         
         serializer.save(content=content)
+        # You can also send a Django signal
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
-# product retrieve View
+# product retrieve API View
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk'
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+# product list API View 
+class ProductListAPIView(generics.ListAPIView):
+    
+    """
+    Not gonna use this method
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+product_list_view = ProductListAPIView.as_view()
 
